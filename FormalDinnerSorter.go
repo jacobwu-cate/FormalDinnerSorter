2e41e59b0fee89222c2afc41f8022e6a6ff6f507
jacobwu-cate <56365132+jacobwu-cate@users.noreply.github.com>
2020-02-09 22:36:10 -0800

    final upload; ready for grading

--- FormalDinnerSorter.go
+++ FormalDinnerSorter.go
@@ -1,10 +1,10 @@
 // Formal Dinner Sorter
 // Jacob Wu
-// 01.28.2020
+// 02.09.2020
 
 package main
 
-import ( // Get packages we need
+import (
 	"bufio"
 	"encoding/csv"
 	"fmt"
@@ -14,46 +14,55 @@ import ( // Get packages we need
 	"os"
   "sort"
   "strconv"
+	"strings"
 	"time"
 )
 
-type person struct { // Every person has:
-	name string // Name
-	id int // ID
-  timesServed int // How many times the student was waiter/kitchen crew
-	haveMet []int // ID numbers of students the person has met
-  currentAssignment string // Current status
+type person struct {
+	name string
+	id int // Unique Identifying Number of the person, used to prevent sitting someone next to the same people
+  timesServed int // Number of times as waiter or kitchen staff
+	haveMet []int // ID numbers of people seated with
+	previousAssignments []string // History of table assignment or staff
+  currentAssignment string // Currently task - table with specified number or staff
 }
 
-type table struct { // Every table has properties:
-  occupants []person // Who sits there
-  disallow []int // This table's people have already met
+type table struct {
+	id int // Unique identifying number of the table
+  occupants []person	// All the people seated at the table
+  disallow []int // Everyone the table's occupants have met before
 }
 
 type ByTimesServed []person // Sort in ascending order by number of times served as a kitchen/waiter staff
 
 func (a ByTimesServed) Len() int           { return len(a) }
 func (a ByTimesServed) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a ByTimesServed) Less(i, j int) bool { return a[i].timesServed < a[j].timesServed }
+func (a ByTimesServed) Less(i, j int) bool { return a[i].timesServed > a[j].timesServed }
+
+type ByPeopleSeated []table // Sort in ascending order by number of times served as a kitchen/waiter staff
+
+func (a ByPeopleSeated) Len() int           { return len(a) }
+func (a ByPeopleSeated) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByPeopleSeated) Less(i, j int) bool { return len(a[i].occupants) < len(a[j].occupants) }
 
 var ( // Global variables
 	people []person // Everyone from the excel sheet
+  tables []table // Keeps track of all tables
+  staffList []person // Keeps track of all staff
   numKitchenStaff = 7 // MUTABLE no. ppl needed at kitchen
   numTables = 31 // MUTABLE no. ppl needed for waiting
   printDetailsForDebug = false // Toggle for detailed info
-  tables []table // Keeps track of all tables
-  staffList []person // Keeps track of all staff
-  kitchenNames []string // String array of kitchen staff
-  waiterNames []string // String array of waiter staff
-  data [][]string // data for CSV export
-)
+	printEssentials = false
+  dataToPrint [][]string // data for CSV export
+) // ### Should have used a map for people variable; will make references easier
 
 func resetVariables() { // Clear existing data for next round of assignments
   tables = tables[:0]
   staffList = make([]person, 0)
-  kitchenNames = make([]string, 0)
-  waiterNames = make([]string, 0)
-  data = make([][]string, 0)
+  dataToPrint = make([][]string, 0)
+	for i := 0; i<len(people); i++ {
+		people[i].previousAssignments = append(people[i].previousAssignments, people[i].currentAssignment)
+	} // Save every student's previous assignment
 }
 
 func getStudentNames() { // This function populates student data
@@ -68,11 +77,11 @@ func getStudentNames() { // This function populates student data
 		} else if error != nil {
 			log.Fatal(error)
 		}
-		people = append(people, person{line[1] + " " + line[0], i, 0, make([]int, 0), ""}) // Add the student
+		people = append(people, person{line[1] + " " + line[0], i, 0, make([]int, 0), make([]string, 0), ""}) // Add the student
 	}
 }
 
-func shuffleStudents(people []person) { // Randomize order of students
+func shuffleStudents() { // Randomize order of students
   rand.Seed(time.Now().UnixNano())
 	for i := len(people) - 1; i > 0; i-- { // Fisher–Yates shuffle
 		j := rand.Intn(i + 1)
@@ -80,70 +89,41 @@ func shuffleStudents(people []person) { // Randomize order of students
 	}
 }
 
-func drawStaff(n int, role string) {
-	sort.Sort(ByTimesServed(people)) // Sort by how many times served as Kitchen/Waiter staff
-	for i := 1;  i<=n; i++ { // Until we get number of staff we need
-    staffList = append(staffList, people[0]) // Take the first person
-    if role == "Kitchen" {
-      kitchenNames = append(kitchenNames, people[0].name)
-    } else {
-      waiterNames = append(waiterNames, people[0].name)
-    } // And write down their name
-    fmt.Println(role + " <- " + people[0].name)
-    people[0].currentAssignment = role
-    people[0].timesServed ++
-    people = append(people, people[0]) // Moves first person to last, since he's been assigned role
-    people = append(people[:0], people[1:]...)
-  }
-  if printDetailsForDebug { fmt.Print("\n", people, "\n\n") }
-}
-
 func sortTables() {
-  tables = make([]table, 50) // Make empty table
-  peoplePerTable := (len(people)-numKitchenStaff-numTables)/numTables // Calculate number of people per table by (Non-staff students)/(Number of tables)
-  if printDetailsForDebug { fmt.Println(peoplePerTable) }
-  tablesWithExtraPerson := (len(people)-numKitchenStaff-numTables)%numTables // Calculate tables with one extra person; remainder from (Non-staff students)/(Number of tables)
-  if printDetailsForDebug { fmt.Println(tablesWithExtraPerson) }
-  for i := 1; i<=numTables; i++ {
-    if i <= tablesWithExtraPerson { // Accomodate the extra person
-      tryStudent(i)
-    }
-    for j := 0; j<peoplePerTable; j++ { // Place number of people at table
-      tryStudent(i)
-    }
-    tableIds := make([]int, 0)
-    var tableNames []string // Keep a list of people at the table, in string format
-    for _,occupant := range tables[i].occupants {
-      tableNames = append(tableNames, occupant.name) // Add name to table's occupant property
-      tableIds = append(tableIds, occupant.id) // Add name to table's id
-    }
-    for k := 1; k<=peoplePerTable; k++ {
-      people[len(people)-k].haveMet = tableIds // Indicate everyone has met everyone at the table (id)
-    }
-    if i <= tablesWithExtraPerson { // If this is the table with one extra person
-      people[len(people)-peoplePerTable-1].haveMet = tableIds // Indicate the extra person has met everyone at the table (id)
-    }
-    data = append(data,tableNames) // Add names to CSV data queue
-    fmt.Print("Table ", i, " <- ")
-    for _,occupant := range tables[i].occupants {
-      fmt.Print(occupant.name)
-      fmt.Print("(", occupant.id, ")  ")
-    }
-    fmt.Print("\n\n")
-  }
+  sort.Sort(ByTimesServed(people)) // Sort students in order of number of served, big first; this way people who have served the least number of times are more likely to become waiters (since staff are picked last)
+  tables = make([]table, numTables) // Make empty table	
+	for i := 0; i < numTables; i++ { // Number all the tables
+		tables[i].id = i+1
+	}
+	
+	for i := 1; i<=(len(people)-numKitchenStaff-numTables); i++ {
+		tryStudent(i)
+	} // Assign students to a table until we have left the number of staff
 }
 
-func tryStudent(tableNo int) {
-  trialNo := 0 // Start trying at the first person
-  for contains(tables[tableNo].disallow, people[trialNo].id) {
-    // The upcoming student has met one of the existing table members
-    trialNo ++ // In that case, pick another student
-  } // If this person have already met someone at the table, try someone else
-  tables[tableNo].disallow = append(tables[tableNo].disallow, people[trialNo].haveMet...) // Incorporate this person's haveMet list into the table's haveMet list
-  tables[tableNo].occupants = append(tables[tableNo].occupants, people[trialNo]) // Add student to table
-  people[trialNo].currentAssignment = "Table " + strconv.Itoa(tableNo)
-  people = append(people, people[trialNo]) // Moves that person to last, since he's been assigned role
-    people = append(people[:trialNo], people[trialNo+1:]...)
+func tryStudent(i int) {
+	sort.Sort(ByPeopleSeated(tables)) // Sort tables by num. people seated, small first, as we start by assigning students to tables with least num. people  
+	tableNo := 0 // Start trying at the first table (smallest num. people)
+  for contains(tables[tableNo].disallow, people[0].id) {
+    // If the upcoming student has met one of the existing table members
+    tableNo ++ // In that's the case, pick another table
+		if tableNo >= numTables { // If the student cannot be accomodated at any table
+  		people = append(people, person{}) // Try another student by deferring this student
+			copy(people[len(people)-i:], people[len(people)-i-1:]) // Move every person back by 1
+			people[len(people)-i-1] = people[0] // Copy first person to last unsorted pos
+			people = append(people[:0], people[1:]...) // Remove the original copy
+			tryStudent(i+1) // Try to seat another student
+		}
+  }
+	// At this point the student is good to be seated at the table
+	people[0].currentAssignment = "Table " + strconv.Itoa(tables[tableNo].id)
+  tables[tableNo].disallow = append(tables[tableNo].disallow, people[0].haveMet...) // Incorporate this person's haveMet list into the table's haveMet list
+  tables[tableNo].occupants = append(tables[tableNo].occupants, people[0]) // Add student to table
+	for _, occupant := range tables[tableNo].occupants {
+		occupant.haveMet = tables[tableNo].disallow // Update every table occupant's haveMet list
+	}
+  people = append(people, people[0]) // Moves that person to last, since he's been assigned role
+  people = append(people[:0], people[1:]...)
 }
 
 func contains(array []int, key int) bool { // Check if there are overlap
@@ -155,76 +135,142 @@ func contains(array []int, key int) bool { // Check if there are overlap
 	return false
 }
 
-func writeStaffNames() { // Export kitchen crew at row 33 and waiters row 34
-  data = append(data, make([]string, 0))
-  data = append(data, kitchenNames)
-  data = append(data, waiterNames)
+func drawStaff(n int, role string) { // Take last 38 students
+	for i := 1;  i<=n; i++ { // Until we get number of staff we need
+    if printEssentials { fmt.Println(role + " <- " + people[0].name) }
+    people[0].currentAssignment = role
+    people[0].timesServed ++
+    staffList = append(staffList, people[0])
+    people = append(people, people[0]) // Moves first person to last, since he's been assigned role
+    people = append(people[:0], people[1:]...)
+  }
+  if printDetailsForDebug { fmt.Print("\n", people, "\n\n") }
+}
+
+func writeToCSV() { // Export kitchen crew at row 33 and waiters row 34
+	for tableNo, table := range tables { // For every table
+		tableNames := []string{"Table " + strconv.Itoa(tableNo+1)}
+		for _, occupant := range table.occupants { // For every person at the table
+			tableNames = append(tableNames, occupant.name) // Include their name
+		}
+		dataToPrint = append(dataToPrint, tableNames) // And add all table names to queue
+	}
+	kitchenNames := []string{"Kitchen Staff"} // Prepare a variable to put all names in
+	waiterNames := []string{"Waiter Staff"} // Prepare a variable to put all names in
+	for _, occupant := range staffList { // For every person at the table
+		if occupant.currentAssignment == "Kitchen" {
+			kitchenNames = append(kitchenNames, occupant.name) // Include their name
+		} else {
+			waiterNames = append(waiterNames, occupant.name)
+		}
+	}
+	dataToPrint = append(dataToPrint, make([]string, 0)) // Include empty line to demarcate diners and staffs
+	dataToPrint = append(dataToPrint, kitchenNames) // Add names of kitchen staff
+	dataToPrint = append(dataToPrint, waiterNames) // Add names of waiter staff
 }
 
 func exportCSV(n string) {
-  fileName := "result" + n + ".csv"
+  fileName := "result" + n + ".csv" // Name the file: Seating 1, 2, 3; or Master
   file, err := os.Create(fileName)
   if err != nil {
     log.Fatal(err)
   }
-  defer file.Close()
+  defer file.Close() // Wait until we finished, then close file
 
   writer := csv.NewWriter(file)
   defer writer.Flush()
 
-  for _, value := range data {
-    err := writer.Write(value)
+  for _, value := range dataToPrint { // For line in data to print
+    err := writer.Write(value) // Write the line
     if err != nil {
       log.Fatal(err)
     }
   }
 }
 
+func writeMasterCSV() {
+	sortPeopleByName() // Sort students by their name
+	dataToPrint = make([][]string, 0)
+	heading := []string{"Name", "ID", "No. Times as Staff", "Seatings 1-3"}
+	dataToPrint = append(dataToPrint, heading)
+	for _, person := range people { // For every student
+		personInfo := []string{person.name, strconv.Itoa(person.id), strconv.Itoa(person.timesServed), strings.Join(person.previousAssignments, " ")}
+		dataToPrint = append(dataToPrint, personInfo) // Include these information
+	}
+}
+
+// Below include substantive reference to golang - sort package website //
+
+type By func(p1, p2 *person) bool
+
+type personSorter struct {
+	people []person
+	by      func(p1, p2 *person) bool // Closure used in the Less method.
+}
+
+func (by By) Sort(people []person) {
+	ps := &personSorter{
+		people: people,
+		by:      by,
+	}
+	sort.Sort(ps)
+}
+
+func (s *personSorter) Len() int {
+	return len(s.people)
+}
+
+func (s *personSorter) Swap(i, j int) {
+	s.people[i], s.people[j] = s.people[j], s.people[i]
+}
+
+func (s *personSorter) Less(i, j int) bool {
+	return s.by(&s.people[i], &s.people[j])
+}
+
+func sortPeopleByName() {
+	name := func(p1, p2 *person) bool {
+		return p1.name < p2.name
+	}
+	By(name).Sort(people)
+}
+
+// Above include substantive reference to golang - sort package website //
+
 func main() {
 	getStudentNames()
-	if printDetailsForDebug { fmt.Print(people, "\n\n") }
-
-  shuffleStudents(people)
   if printDetailsForDebug { fmt.Print(people, "\n\n") }
 
-  drawStaff(numKitchenStaff, "Kitchen")
-  drawStaff(numTables, "Waiter")
+	for i := 1; i<=3; i++ {
+		shuffleStudents()
+  	sortTables()
 
-  sortTables()
-  writeStaffNames()
+  	drawStaff(numKitchenStaff, "Kitchen")
+  	drawStaff(numTables, "Waiter")
 
-  exportCSV("1")
-  fmt.Print("Finished run without error\n\n")
+  	writeToCSV()
+  	exportCSV(strconv.Itoa(i))
 
-  resetVariables()
-////////////////////////
-  shuffleStudents(people)
-  if printDetailsForDebug { fmt.Print(people, "\n\n") }
+  	if printEssentials { fmt.Print("Finished run without error\n\n") }
 
-  drawStaff(numKitchenStaff, "Kitchen")
-  drawStaff(numTables, "Waiter")
+		if printDetailsForDebug {
+			for _, table := range tables {
+				fmt.Println(table)
+			}
+		}
 
-  sortTables()
-  writeStaffNames()
+	  resetVariables()
+	}
+	writeMasterCSV()
+	exportCSV("Master")
+}
 
-  exportCSV("2")
-  fmt.Print("Finished run without error")
-
-  resetVariables()
-////////////////////////
-  shuffleStudents(people)
-  if printDetailsForDebug { fmt.Print(people, "\n\n") }
-
-  drawStaff(numKitchenStaff, "Kitchen")
-  drawStaff(numTables, "Waiter")
-
-  sortTables()
-  writeStaffNames()
-
-  exportCSV("3")
-  fmt.Print("Finished run without error")
-
-  resetVariables()
+// Below are auxiliary functions helpful for debugging
+func printStudentID() {
+	for _, element := range people {
+		fmt.Print(element.id, " ")
+	}
+	fmt.Print("\n\n")
 }
 
 // References
@@ -237,3 +283,7 @@ func main() {
 // {7} https://golang.org/pkg/sort/
 // {8} https://ispycode.com/GO/Collections/Arrays/Check-if-item-is-in-array
 // {9} https://stackoverflow.com/questions/10105935/how-to-convert-an-int-value-to-string-in-go
+// {10} https://stackoverflow.com/questions/46128016/insert-value-in-a-slice-at-given-index
+// {11} https://www.dotnetperls.com/convert-slice-string-go
+
+// Note: There is significant reference from the sort package on the Go website (7)
